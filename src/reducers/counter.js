6fc35e0407737733a1d1c3a14c02c1a5338d0930
lkmSasanga <lkmsasanga@gmail.com>
2021-02-24 17:34:57 +0530

    counter reducer implemented

---
+++ src/reducers/counter.js
@@ -0,0 +1,11 @@
+const counterReducer = (state = 0, action) => {
+    // eslint-disable-next-line default-case
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+    }
+}
+
+export default counterReducer;
